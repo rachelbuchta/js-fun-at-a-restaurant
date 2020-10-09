ac8b04986853ec446c29cd2ee1c47c99d3c0f63f
Rachel Buchta <rachelbuchta@yahoo.com>
2020-10-08 21:18:09 -0600

    Add meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,58 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+    return `Delicious ${menuItemName}`
+};
 
-}
+function createMenuItem(menuItemName, price, type) {
+    var menuItemName = {
+      name : menuItemName,
+      price : price,
+      type : type
+    }
+
+    return menuItemName;
+};
+
+function addIngredients(ingredientName, ingredients) {
+      if (ingredients.includes(ingredientName) !== true) {
+      ingredients.push(ingredientName);
+      } else {
+      return ingredients;
+    }
+  };
+
+function formatPrice(initialPrice) {
+    return "$" + initialPrice;
+};
+
+function decreasePrice(initialPrice) {
+      var decreasedPrice = (initialPrice/10);
+      return (initialPrice - decreasedPrice);
+    };
+
+function createRecipe(title, ingredients, menuItemType) {
+    var recipe = {
+      title : title,
+      ingredients : ingredients,
+      type : menuItemType
+    }
+
+    return recipe;
+};
 
 
-module.exports = {
+
+
+
+
+
+
+
+
+  module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
