5303ac47af97315e885c7a0002f9ac6f4476123c
Rachel Buchta <rachelbuchta@yahoo.com>
2020-10-10 11:37:28 -0600

    Add order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,57 @@
 
+function takeOrder(newOrder, deliveryOrders) {
+
+    if (newOrder.status === 'accepted') {
+      deliveryOrders.push(newOrder);
+    };
+
+    if (deliveryOrders.length > 3) {
+      deliveryOrders.pop();
+    };
+
+    return deliveryOrders;
+
+};
+
+function refundOrder(newOrder, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber == newOrder) {
+      deliveryOrders.splice(i,1);
+    }
+  }
+};
+
+
+
+function listItems(deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    var items = `${deliveryOrders[0].item}, ${deliveryOrders[1].item}, ${deliveryOrders[2].item}`;
+  }
+  return items;
+};
+
+
+
+function searchOrder(deliveryOrders, newOrder) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === newOrder) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+
+
+
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
