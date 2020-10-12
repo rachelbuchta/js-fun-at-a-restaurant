717526a76c4dac9648d930e9b534008b0823499c
Rachel Buchta <rachelbuchta@yahoo.com>
2020-10-11 18:18:01 -0600

    Add final order

--- src/order.js
+++ src/order.js
@@ -1,32 +1,30 @@
-
 function takeOrder(newOrder, deliveryOrders) {
+  if (newOrder.status === 'accepted') {
+    deliveryOrders.push(newOrder);
+  };
 
-    if (newOrder.status === 'accepted') {
-      deliveryOrders.push(newOrder);
-    };
+  if (deliveryOrders.length > 3) {
+    deliveryOrders.pop();
+  };
 
-    if (deliveryOrders.length > 3) {
-      deliveryOrders.pop();
-    };
-
-    return deliveryOrders;
-
+  return deliveryOrders;
 };
+
 
 function refundOrder(newOrder, deliveryOrders) {
   for (var i = 0; i < deliveryOrders.length; i++) {
     if (deliveryOrders[i].orderNumber == newOrder) {
-      deliveryOrders.splice(i,1);
-    }
-  }
+      deliveryOrders.splice(i, 1);
+    };
+  };
 };
-
 
 
 function listItems(deliveryOrders) {
   for (var i = 0; i < deliveryOrders.length; i++) {
     var items = `${deliveryOrders[0].item}, ${deliveryOrders[1].item}, ${deliveryOrders[2].item}`;
-  }
+  };
+
   return items;
 };
 
@@ -36,8 +34,8 @@ function searchOrder(deliveryOrders, newOrder) {
   for (var i = 0; i < deliveryOrders.length; i++) {
     if (deliveryOrders[i].item === newOrder) {
       return true;
-    }
-  }
+    };
+  };
   return false;
 };
 
@@ -50,8 +48,8 @@ function searchOrder(deliveryOrders, newOrder) {
 
 
 module.exports = {
-   takeOrder,
-   refundOrder,
-   listItems,
-   searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
