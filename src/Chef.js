1c92bcd7ed76444f67f7eafaac06c237852c0915
Rachel Buchta <rachelbuchta@yahoo.com>
2020-10-11 18:22:25 -0600

    Add final chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,23 @@
 class Chef {
-  constructor() {
-    
-  }
-}
+  constructor(chefName, newRestaurant) {
+    this.name = chefName;
+    this.restaurant = newRestaurant;
+    this.greetCustomer = function(newCustomer, isMorning) {
+      if (isMorning == true) {
+        return `Good morning, ${newCustomer}!`
+      } else {
+        return `Hello, ${newCustomer}!`
+      };
+    };
+    this.checkForFood = function(foodItem) {
+      if (newRestaurant.menus.breakfast.includes(foodItem) || newRestaurant.menus.lunch.includes(foodItem)) {
+        return `Yes, we're serving ${foodItem.name} today!`;
+      };
+
+        return `Sorry, we aren't serving ${foodItem.name} today.`;
+    };
+  };
+};
+
 
 module.exports = Chef;
